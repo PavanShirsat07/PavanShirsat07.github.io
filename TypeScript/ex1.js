02f590f6447acb478ba935b116d39ff1a960d663
Pavan Shirsat <pavanshirsat957@gmail.com>
2025-06-10 11:36:42 +0530

    crudapi

--- TypeScript/ex1.js
+++ TypeScript/ex1.js
@@ -1,2 +1,7 @@
+"use strict";
 var newMessage = "Hello, TypeScript!";
 console.log(newMessage);
+function greet(name) {
+    return `Hello, ${name}!`;
+}
+console.log(greet("Pavan Shirsat"));
--- TypeScript/ex1.ts
+++ TypeScript/ex1.ts
@@ -1,2 +1,7 @@
 var newMessage:string = "Hello, TypeScript!";
 console.log(newMessage);
+
+function greet(name: string): string {
+    return `Hello, ${name}!`;
+}
+console.log(greet("Pavan Shirsat"));
