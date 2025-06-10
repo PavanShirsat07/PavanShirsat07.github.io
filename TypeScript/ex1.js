a747520a2032ad196526aafe08a53a50a70d5233
Pavan Shirsat <pavanshirsat957@gmail.com>
2025-06-10 14:33:53 +0530

    typescript

--- TypeScript/ex1.js
+++ TypeScript/ex1.js
@@ -1,7 +1,26 @@
 "use strict";
 var newMessage = "Hello, TypeScript!";
 console.log(newMessage);
-function greet(name) {
-    return `Hello, ${name}!`;
-}
-console.log(greet("Pavan Shirsat"));
+// function greet(name: string): string {
+//     return `Hello, ${name}!`;
+// }
+// console.log(greet("Pavan Shirsat"));
+let a = [1, 2, 3, 4, 5];
+let b = a;
+b.push(6);
+// console.log(a); // Output: [1, 2, 3, 4, 5, 6]
+//Tuple example
+let tuple = [1, "Hello", 10, "World"];
+// console.log(tuple[0]); // Output: 1
+//Array example
+let arr = [10, "pavan", true, 1, 2, 3];
+// console.log(arr[0]); // Output: 10
+//Enum example
+var Color;
+(function (Color) {
+    Color["Red"] = "Red";
+    Color["Green"] = "Green";
+    Color["Blue"] = "Blue";
+})(Color || (Color = {}));
+let mycolor = Color.Red;
+// console.log(mycolor); // Output: Red
--- TypeScript/ex1.ts
+++ TypeScript/ex1.ts
@@ -1,7 +1,31 @@
 var newMessage:string = "Hello, TypeScript!";
 console.log(newMessage);
 
-function greet(name: string): string {
-    return `Hello, ${name}!`;
+// function greet(name: string): string {
+//     return `Hello, ${name}!`;
+// }
+// console.log(greet("Pavan Shirsat"));
+ 
+let a=[1,2,3,4,5];
+let b=a;
+b.push(6);
+// console.log(a); // Output: [1, 2, 3, 4, 5, 6]
+
+//Tuple example
+let tuple:[number,string,number,string] = [1, "Hello",10,"World"];
+// console.log(tuple[0]); // Output: 1
+
+//Array example
+let arr: (number | string | boolean)[] = [10, "pavan", true, 1, 2, 3];
+// console.log(arr[0]); // Output: 10
+
+//Enum example
+enum Color {
+    Red = "Red",
+    Green = "Green",
+    Blue = "Blue"
 }
-console.log(greet("Pavan Shirsat"));
+let mycolor:Color = Color.Red;
+// console.log(mycolor); // Output: Red
+
+ 
